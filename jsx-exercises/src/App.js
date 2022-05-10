d2b60c178c943fb276a40f2eaca662966bcd2dc9
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-05-10 15:26:28 +0200

    feat/exercise jsx-02 completed

--- jsx-exercises/src/App.js
+++ jsx-exercises/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GreedingName } from './Jsx-02';
 
 export class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <GreedingName name='Francesco' />
+      </div>
+    );
   }
 }
---
+++ jsx-exercises/src/Jsx-02.js
@@ -0,0 +1,3 @@
+export function GreedingName({ name }) {
+  return <h1>{`Hello, ${name}`}</h1>;
+}
